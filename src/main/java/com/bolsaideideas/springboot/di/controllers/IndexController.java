a65c2d3ff66005bfe6afd443a5c6ea46c9d9913c
henryhaller99 <henryhernandezbarajas@outlook.com>
2023-08-12 17:29:58 -0600

    index.html template added in order to use MiServicio.java class

--- src/main/java/com/bolsaideideas/springboot/di/controllers/IndexController.java
+++ src/main/java/com/bolsaideideas/springboot/di/controllers/IndexController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bolsaideideas.springboot.di.models.services.MiServicio;
+
 @Controller
 public class IndexController {
 	
+	private MiServicio miservicio = new MiServicio();
+	
 	@GetMapping({"/","/index",""})
 	public String index(Model model) {
+		model.addAttribute("objeto",miservicio.operacion());
+		
 		return "index";
 	}
 
