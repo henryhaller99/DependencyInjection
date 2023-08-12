7a488de24225a7ff658fe2e6c786f6b2a14855b8
henryhaller99 <henryhernandezbarajas@outlook.com>
2023-08-12 17:21:06 -0600

    Clase Service añadida

---
+++ src/main/java/com/bolsaideideas/springboot/di/models/services/MiServicio.java
@@ -0,0 +1,10 @@
+package com.bolsaideideas.springboot.di.models.services;
+
+
+// interactua con los DAO, o se comunican con las APIs REST para obtener los datos de algún otro microservicio
+public class MiServicio {
+
+	public String operacion() {
+		return "se está ejecutando alguna operación importante....";
+	}
+}
